660d5187e27c9384e91e17f3ddefac8dfb556090
Kaskin Suporte <kaskin_suporte@outlook.com>
2022-08-24 10:05:51 -0300

    Impressão funcionando

--- ProjetoAcessibilidade/Views/PrintPage.xaml.cs
+++ ProjetoAcessibilidade/Views/PrintPage.xaml.cs
@@ -11,6 +11,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Printing;
 
 using ProjetoAcessibilidade.Services;
 using ProjetoAcessibilidade.ViewModels;
@@ -28,49 +29,145 @@ namespace ProjetoAcessibilidade.Views;
 /// </summary>
 public sealed partial class PrintPage : Page
 {
-    private PrintHelper printHelper;
+    private PrintManager printMan;
+    private PrintDocument printDoc;
+    private IPrintDocumentSource printDocSource;
     public PrintViewModel ViewModel
     {
         get; private set;
     }
-    private async void OnPrintButtonClick(object sender, RoutedEventArgs e)
+
+    public PrintPage()
     {
-        await printHelper.ShowPrintUIAsync();
+        InitializeComponent();
+        RegisterPrint();
     }
 
+    #region Register for printing
 
-    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    void RegisterPrint()
     {
-        if (printHelper != null)
-        {
-            printHelper.UnregisterForPrinting();
-        }
+        // Register for PrintTaskRequested event
+        var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
+
+        printMan = PrintManagerInterop.GetForWindow(hWnd);
+        printMan.PrintTaskRequested += PrintTaskRequested;
+
+        // Build a PrintDocument and register for callbacks
+        printDoc = new PrintDocument();
+        printDocSource = printDoc.DocumentSource;
+        printDoc.Paginate += Paginate;
+        printDoc.GetPreviewPage += GetPreviewPage;
+        printDoc.AddPages += AddPages;
     }
 
-    private void Page_Loaded(object sender, RoutedEventArgs e)
+    #endregion
+
+    #region Showing the print dialog
+
+    private async void PrintButtonClick(object sender, RoutedEventArgs e)
     {
         if (PrintManager.IsSupported())
         {
-            // Tell the user how to print
-            //MainPage.Current.NotifyUser("Print contract registered with customization, use the Print button to print.", NotifyType.StatusMessage);
+            try
+            {
+                // Show print UI
+                var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
+                await PrintManagerInterop.ShowPrintUIForWindowAsync(hWnd);
+            }
+            catch
+            {
+                // Printing cannot proceed at this time
+                ContentDialog noPrintingDialog = new ContentDialog()
+                {
+                    XamlRoot = (sender as Button).XamlRoot,
+                    Title = "Printing error",
+                    Content = "\nSorry, printing can' t proceed at this time.",
+                    PrimaryButtonText = "OK"
+                };
+                await noPrintingDialog.ShowAsync();
+            }
         }
         else
         {
-            // Remove the print button
-            InvokePrintingButton.Visibility = Visibility.Collapsed;
-
-            // Inform user that Printing is not supported
-            //MainPage.Current.NotifyUser("Printing is not supported.", NotifyType.ErrorMessage);
-
-            // Printing-related event handlers will never be called if printing
-            // is not supported, but it's okay to register for them anyway.
+            // Printing is not supported on this device
+            ContentDialog noPrintingDialog = new ContentDialog()
+            {
+                XamlRoot = (sender as Button).XamlRoot,
+                Title = "Printing not supported",
+                Content = "\nSorry, printing is not supported on this device.",
+                PrimaryButtonText = "OK"
+            };
+            await noPrintingDialog.ShowAsync();
         }
-
-        // Initalize common helper class and register for printing
-        printHelper = new PrintHelper(this);
-        printHelper.RegisterForPrinting();
-
-        // Initialize print content for this scenario
-        printHelper.PreparePrintContent(new PageToPrint());
     }
+
+    private void PrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs args)
+    {
+        // Create the PrintTask.
+        // Defines the title and delegate for PrintTaskSourceRequested
+        var printTask = args.Request.CreatePrintTask("Print", PrintTaskSourceRequrested);
+
+        // Handle PrintTask.Completed to catch failed print jobs
+        printTask.Completed += PrintTaskCompleted;
+    }
+
+    private void PrintTaskSourceRequrested(PrintTaskSourceRequestedArgs args)
+    {
+        // Set the document source.
+        args.SetSource(printDocSource);
+    }
+
+    #endregion
+
+    #region Print preview
+
+    private void Paginate(object sender, PaginateEventArgs e)
+    {
+        // As I only want to print one Rectangle, so I set the count to 1
+        printDoc.SetPreviewPageCount(1, PreviewPageCountType.Final);
+    }
+
+    private void GetPreviewPage(object sender, GetPreviewPageEventArgs e)
+    {
+        // Provide a UIElement as the print preview.
+        printDoc.SetPreviewPage(e.PageNumber, new PageToPrint());
+    }
+
+    #endregion
+
+    #region Add pages to send to the printer
+
+    private void AddPages(object sender, AddPagesEventArgs e)
+    {
+        printDoc.AddPage(new PageToPrint());
+
+        // Indicate that all of the print pages have been provided
+        printDoc.AddPagesComplete();
+    }
+
+    #endregion
+
+    #region Print task completed
+
+    private void PrintTaskCompleted(PrintTask sender, PrintTaskCompletedEventArgs args)
+    {
+        // Notify the user when the print operation fails.
+        if (args.Completion == PrintTaskCompletion.Failed)
+        {
+            this.DispatcherQueue.TryEnqueue(async () =>
+            {
+                ContentDialog noPrintingDialog = new ContentDialog()
+                {
+                    XamlRoot = this.Content.XamlRoot,
+                    Title = "Printing error",
+                    Content = "\nSorry, failed to print.",
+                    PrimaryButtonText = "OK"
+                };
+                await noPrintingDialog.ShowAsync();
+            });
+        }
+    }
+
+    #endregion
 }
