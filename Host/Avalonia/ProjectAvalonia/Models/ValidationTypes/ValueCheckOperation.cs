604d29858fdcd9381343cd40f557460b1c98d126
Fabricio Gonzalez <fabriciogonzalezfr@gmail.com>
2023-06-30 20:41:20 -0300

    Finalizando projeto

---
+++ Host/Avalonia/ProjectAvalonia/Models/ValidationTypes/ValueCheckOperation.cs
@@ -0,0 +1,18 @@
+﻿using ProjectAvalonia.Common.Extensions;
+using ProjectAvalonia.Presentation.Interfaces;
+using ReactiveUI;
+
+namespace ProjectAvalonia.Models.ValidationTypes;
+
+public class ValueCheckOperation : ReactiveObject, IOperationType
+{
+    public string Value
+    {
+        get;
+    } = "ValueCheck";
+
+    public string LocalizationKey
+    {
+        get;
+    } = "ValueCheckLabel".GetLocalized();
+}
