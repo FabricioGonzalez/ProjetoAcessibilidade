77b02e8a70f8b055b55028ed1471e635fd06df80
Fabricio Gonzalez <fabriciogonzalezfr@gmail.com>
2023-07-19 20:05:52 -0300

    Alterações e correções avalonia 11

--- Host/Avalonia/ProjectAvalonia/Common/Helpers/IconExtensions.cs
+++ Host/Avalonia/ProjectAvalonia/Common/Helpers/IconExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using Avalonia;
-using Avalonia.Markup.Xaml.Styling;
+﻿using Avalonia;
 using Avalonia.Media;
 using Common.Optional;
 
@@ -10,18 +8,35 @@ public static class IconExtensions
 {
     public static StreamGeometry GetIcon(
         this string iconKey
-    ) =>
-        Application.Current.Styles.FirstOrDefault(i => i is StyleInclude style
-                                                       && style.Source.AbsolutePath.Equals("/Icons/IconsSmall.axaml"))
-            .ToOption()
+    )
+    {
+        /*var icon =*/
+        if (Application.Current?
+                .Styles
+                .TryGetResource(key: iconKey
+                    , theme: null, value: out var iconOpen) == true)
+
+        {
+            return iconOpen.ToOption()
+                .Map(i => i as StreamGeometry)
+                .Reduce(() => StreamGeometry.Parse(""));
+        }
+
+        return StreamGeometry.Parse("");
+        /*.FirstOrDefault(i => i is StyleInclude style
+                             && style.Source.AbsolutePath.Equals(
+                                 "/Icons/IconsSmall.axaml"));*/
+
+        /*return icon.ToOption()
             .Map(iconResource =>
             {
                 (iconResource as StyleInclude).TryGetResource(key: iconKey
-                    , theme: Application.Current.RequestedThemeVariant, value: out var iconOpen);
+                    , theme: null, value: out var iconOpen);
 
                 return iconOpen.ToOption()
                     .Map(i => i as StreamGeometry)
                     .Reduce(() => StreamGeometry.Parse(""));
             })
-            .Reduce(() => StreamGeometry.Parse(""));
+            .Reduce(() => StreamGeometry.Parse(""));*/
+    }
 }
